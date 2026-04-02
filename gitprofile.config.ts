da205fa943c239fa759fc49647fb699a17971cfb
n-furtado <furtadonina6@gmail.com>
2026-04-02 10:43:04 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -122,7 +122,7 @@ const CONFIG = {
       link: 'https://example.com',
     },
   ],
-  education: [
+  educations: [
     {
       institution: 'California State University Stanislaus',
       degree: 'Biological Sciences',
